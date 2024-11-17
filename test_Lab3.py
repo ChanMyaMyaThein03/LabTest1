00ced05cc97f369011463d832d1f26918d28a62e
Chan Mya Mya Thein <chanmmthein03.23@ichat.sp.edu.sg>
2024-11-17 23:37:44 +0800

    Updated Testing Lab 3

--- test_Lab3.py
+++ test_Lab3.py
@@ -2,14 +2,12 @@ import Lab3
 
 print("Test_Lab3")
 
-
 def test_bubble_sort_ascending():
     result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
     test_arr = [11, 12, 22, 25, 34, 64, 90]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
-
     assert (result == test_arr)
 
 def test_bubble_sort_descending():
@@ -18,7 +16,6 @@ def test_bubble_sort_descending():
     test_arr = [90, 64, 34, 25, 22, 12, 11]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
-
     assert (result == test_arr)
 
 def test_bubble_sort_invalid():
@@ -26,7 +23,6 @@ def test_bubble_sort_invalid():
     input_arr = [64, 34, 25, 12, 22, 11, 90]
 
     result = Lab3.bubble_sort(input_arr, 3)
-
     assert (result == [])
 
 def test_bubble_sort_greaterThan10():
@@ -34,5 +30,21 @@ def test_bubble_sort_greaterThan10():
     input_arr = [64, 34, 25, 12, 22, 11, 90, 25, 48, 30]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
-    assert ( result == 1)
+    assert (result == 1)
+
+def test_bubble_sort_emptyList():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert( result == 0)
+
+def test_bubble_sort_notInteger():
+    result = []
+    input_arr = [64, '34', 25, 12, 22, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert( result == 2)
+
+
 
