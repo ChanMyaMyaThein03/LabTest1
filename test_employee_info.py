1cf6882bd56a0007eec7366291db3c1deb3f3d90
Chan Mya Mya Thein <chanmmthein03.23@ichat.sp.edu.sg>
2024-11-18 01:17:53 +0800

    Updated File

--- test_employee_info.py
+++ test_employee_info.py
@@ -8,3 +8,10 @@ def test_get_employees_by_age_range():
     result = employee_info.get_employees_by_age_range(age_lower_limit, age_upper_limit)
     test = [ {"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000}]
     assert(result == test)
+
+def test_calculate_average_salary():
+    test = 60166.67
+    result = employee_info.calculate_average_salary()
+
+    assert(result == test)
+
